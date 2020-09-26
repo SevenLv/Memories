ab2f1eaa169d7828fda796a212278208aff6026a
Seven Lv <lvhongqiang@msn.cn>
2020-09-26 15:49:44 +0800

    add first benchmark test console

---
+++ src/Memories.Benchmark/InitializationTester.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Mathematics;
+
+namespace Seven.Memories.Benchmark
+{
+    [SimpleJob(RuntimeMoniker.CoreRt31)]
+    [MarkdownExporterAttribute.GitHub]
+    [RankColumn(NumeralSystem.Arabic)]
+    public class InitializationTester
+    {
+        #region properties
+        [Params(10, 100, 1000, 10000,100000)]
+        public int Count { get; set; }
+
+        [Params(1024, 2048, 10240, 20480)]
+        public int Size { get; set; }
+        #endregion properties
+
+        #region methods
+        [Benchmark]
+        public void Initialize()
+        {
+            using var pool = new MemoryPool(Count, Count, Size);
+        }
+        #endregion methods
+    }
+}
---
+++ src/Memories.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using Seven.Memories.Benchmark;
+using System;
+
+namespace Memories.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<InitializationTester>();
+            Console.ReadLine();
+        }
+    }
+}
